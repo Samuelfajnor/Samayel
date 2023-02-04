ca0e3b3743bb5c9af521d62eca7b3856d6bdf434
Samuelfajnor <51970488+Samuelfajnor@users.noreply.github.com>
2023-02-04 12:19:47 +0100

    Update script-kyanotypia-cursor.js

--- script-kyanotypia-cursor.js
+++ script-kyanotypia-cursor.js
@@ -1,6 +1,6 @@
 document.body.addEventListener("mousemove", evt => {
-  const mouseX = evt.clientX;
-  const mouseY = evt.clientY;
+  const mouseX = evt.clientX -30;
+  const mouseY = evt.clientY -30;
 
   gsap.set(".cursor", {
     x: mouseX,
