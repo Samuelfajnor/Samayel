cf93f5b3509bbde3e1f73bd163eb8a08b539997a
Samuelfajnor <51970488+Samuelfajnor@users.noreply.github.com>
2023-02-06 21:33:47 +0100

    Add files via upload

---
+++ 2018-Mapping-Lovely-Experience/script-mapping-Lovely-Experience-2018-cursor.js
@@ -0,0 +1,12 @@
+document.body.addEventListener("mousemove", evt => {
+  const mouseX = evt.clientX;
+  const mouseY = evt.clientY;
+
+  gsap.set(".cursor", {
+    x: mouseX,
+    y: mouseY });
+
+});
+
+
+
